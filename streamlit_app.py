58d60e24a62f56dc5cd817bca98d9b20d5e05c8b
mrwei <weiguoting.njust.edu.cn>
2025-06-11 12:38:06 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import sys, os
 sys.path.insert(0, os.path.dirname(__file__))
 
 
-from excell.extract_and_split_template import main
+from extract_and_split_template import main
 
 st.title("Excel 智能提取 & 性别拆分")
 
